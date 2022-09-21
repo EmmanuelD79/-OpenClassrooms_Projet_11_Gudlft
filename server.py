9ab5638a503e367bf6e56325ad3adc28238c2a38
EmmanuelD79 <e.demeulnaere@gmail.com>
2022-09-21 14:28:12 +0200

    Bug 5 resolved

--- server.py
+++ server.py
@@ -19,6 +19,7 @@ app.secret_key = 'something_special'
 
 competitions = loadCompetitions()
 clubs = loadClubs()
+MAX_PLACES_PER_CLUB = 12
 
 @app.route('/')
 def index():
@@ -52,9 +53,14 @@ def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-    flash('Great-booking complete!')
-    return render_template('welcome.html', club=club, competitions=competitions)
+    if placesRequired <= int(club["points"]):
+        competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
+        flash(f'Great-booking complete !')
+        return render_template('welcome.html', club=club, competitions=competitions)
+    else:
+        flash("You should not book more than yours available points")
+        response = make_response(render_template('welcome.html', club=club, competitions=competitions))
+        return response, 403
 
 
 # TODO: Add route for points display
